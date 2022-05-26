11aa6f2511014d5cc324786cb2ffd705738a3149
edwardz8 <edwardz8@yahoo.com>
2022-05-26 16:30:20 -0400

    initial commit

---
+++ pages/pitchers.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <ContentRenderer :value="data" />
+  </main>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("pitchers", () =>
+  queryContent("/pitchers").find()
+);
+console.log(data);
+</script>
